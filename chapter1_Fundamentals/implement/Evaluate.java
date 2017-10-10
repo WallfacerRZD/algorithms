d17c08398259b84ac15e9859a44bd82d73227331
WallfacerRZD <1048058400@qq.com>
2017-10-10 17:10:19 +0800

    fix bugs

--- chapter1_Fundamentals/implement/Evaluate.java
+++ chapter1_Fundamentals/implement/Evaluate.java
@@ -2,42 +2,38 @@ package chapter1_Fundamentals.implement;
 
 public class Evaluate {
 	public static void main(String[] args) {
-		String expre = "((1 + 2) * 3)";
+		String expre = "( ( ( 234235 + 5.28 ) * ( 12423 - 8974 ) ) / ( 123 - 235232 ) )";
 		System.out.println(getValue(expre));
 	}
-	
-	private static int getValue(String expression){
-		MyStack<Character> ops = new MyStack(); 
-		MyStack<Integer> vals = new MyStack();
-		for (int i = 0; i < expression.length(); ++i) {
-			char c = expression.charAt(i);
-			if (c != ' '){
-				if (c == '(')             {               }
-				else if (c == ')'){
-					int v = vals.pop();
-					char op = ops.pop();
-					if (op == '+')       {v += vals.pop();}
-					else if(op == '-')   {v -= vals.pop();}
-					else if(op == '*')   {v *= vals.pop();}
-					else                 {v /= vals.pop();}
-					vals.push(v);
+
+	private static double getValue(String expression) {
+		MyStack<String> ops = new MyStack();
+		MyStack<Double> vals = new MyStack();
+		String[] str_array = expression.split(" ");
+		for (int i = 0; i < str_array.length; ++i) {
+			String c = str_array[i];
+			if (c.equals("(")) {
+			} else if (c.equals(")")) {
+				double v = vals.pop();
+				String op = ops.pop();
+				if (op.equals("+")) {
+					v = vals.pop() + v;
+				} else if (op.equals("-")) {
+					v = vals.pop() - v;
+				} else if (op.equals("*")) {
+					v = vals.pop() * v;
+				} else {
+					v = vals.pop() / v;
 				}
-				else if(c == '+'||
-						c == '-'||
-						c == '*'||
-						c == '/'){
-					ops.push(c);
-				}
-				else{
-					vals.push(c - '0');
-				}
-				
+				vals.push(v);
+			} else if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) {
+				ops.push(c);
+			} else {
+				vals.push(Double.parseDouble(c));
 			}
-			else{
-				
-			}
+
 		}
-		assert(vals.size() == 1);
+		assert vals.size() == 1;
 		return vals.pop();
 	}
 }
