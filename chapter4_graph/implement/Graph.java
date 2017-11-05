0fea3b8355ebdfab69c1758143355555960ecdb7
WallfacerRZD <1048058400@qq.com>
2017-11-05 16:26:12 +0800

    begin Graph

---
+++ chapter4_graph/implement/Graph.java
@@ -0,0 +1,44 @@
+package chapter4_graph.implement;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Graph {
+    private final int V;
+    private int E;
+    private LinkedList<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (LinkedList<Integer>[]) new LinkedList[V];
+        // 初始化链表数组的每个元素
+        for (int i = 0; i < adj.length; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
+
+    public int V() {
+        return this.V;
+    }
+
+    public int E() {
+        return this.E;
+    }
+
+    public void addEdge(int v, int w){
+        adj[v].add(w);
+        adj[w].add(v);
+        ++E;
+    }
+
+    public Iterable<Integer> getAdjAt(int v){
+        return adj[v];
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
