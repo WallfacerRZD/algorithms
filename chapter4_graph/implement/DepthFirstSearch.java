d0857a5fa91652112b85592dc5a236a5b1eab862
WallfacerRZD <1048058400@qq.com>
2017-11-23 12:26:57 +0800

    Test DepthFirstSearch

--- chapter4_graph/implement/DepthFirstSearch.java
+++ chapter4_graph/implement/DepthFirstSearch.java
@@ -6,9 +6,9 @@ public class DepthFirstSearch {
 
     public DepthFirstSearch(Graph g, int s) {
         marked = new boolean[g.V()];
-        for (int i = 0; i < marked.length; ++i) {
-            marked[i] = false;
-        }
+//        for (int i = 0; i < marked.length; ++i) {
+//            marked[i] = false;
+//        }
         dfs(g, s);
     }
 
@@ -26,5 +26,21 @@ public class DepthFirstSearch {
     }
     public int count(){
         return count;
+    }
+
+    public static void main(String[] args){
+        Graph test = new Graph("F:\\code\\java\\algorithms\\algs4-data\\tinyG.txt");
+        DepthFirstSearch dfs = new DepthFirstSearch(test, 9);
+        for (int i = 0; i < test.V(); ++i){
+            if (dfs.marked(i)){
+                System.out.print(i + " ");
+            }
+        }
+        if (dfs.count() == test.V()){
+            System.out.println("connected!!");
+        }
+        else{
+            System.out.println("not!!");
+        }
     }
 }
--- chapter4_graph/implement/Graph.java
+++ chapter4_graph/implement/Graph.java
@@ -1,11 +1,15 @@
 package chapter4_graph.implement;
 
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Graph {
-    private final int V;
+    private int V;
     private int E;
     private LinkedList<Integer>[] adj;
 
@@ -19,6 +23,31 @@ public class Graph {
         }
     }
 
+    public Graph(String path) {
+        try {
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            this.V = Integer.parseInt(br.readLine());
+            int E = Integer.parseInt(br.readLine());
+            adj = (LinkedList<Integer>[]) new LinkedList[V];
+            // 初始化链表数组的每个元素
+            for (int i = 0; i < adj.length; ++i) {
+                adj[i] = new LinkedList();
+            }
+
+            for (int i = 0; i < E; ++i){
+                String[] data = br.readLine().split(" ");
+                int data1 = Integer.parseInt(data[0]);
+                int data2 = Integer.parseInt(data[1]);
+                addEdge(data1, data2);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public int V() {
         return this.V;
     }
@@ -27,13 +56,30 @@ public class Graph {
         return this.E;
     }
 
-    public void addEdge(int v, int w){
+    public void addEdge(int v, int w) {
         adj[v].add(w);
         adj[w].add(v);
         ++E;
     }
 
-    public Iterable<Integer> getAdjAt(int v){
+    public Iterable<Integer> getAdjAt(int v) {
         return adj[v];
     }
+
+        // 读取文本文件
+//    public static void main(String[] args) {
+//        String path = "F:\\code\\java\\algorithms\\algs4-data\\tinyG.txt";
+//        try {
+//            FileReader fr = new FileReader(path);
+//            BufferedReader br = new BufferedReader(fr);
+//            String str = "";
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                str += line;
+//            }
+//            System.out.println(str);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
