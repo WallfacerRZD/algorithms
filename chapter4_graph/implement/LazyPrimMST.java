f3151afe3498d6ea8aa5a7b783eda65c9851cbf1
WallfacerRZD <1048058400@qq.com>
2018-02-02 20:47:47 +0800

    注释

--- chapter4_graph/implement/LazyPrimMST.java
+++ chapter4_graph/implement/LazyPrimMST.java
@@ -7,7 +7,15 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
- * 
+ * 延时Prim算法, 寻找最小生成树
+ * 空间复杂度O(E), 时间复杂度O(ElogE)
+ *
+ * 思路: visit(v): 标记v, 把v邻接的边添加到优先队列
+ *      每次从优先队列里取出一条边(边权最小)加入到最小生成树队列
+ * 注意: 检查边是否已无效(两个点都被标记)
+ *
+ * @author RZD
+ * @date 2018/2/2 
  */
 public class LazyPrimMST {
     private boolean[] marked;
